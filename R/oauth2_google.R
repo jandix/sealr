0ffcffc0c5b781b66025007d8a47e16a2a75920b
Jan Dix <dix.jan@gmx.de>
2019-03-04 20:03:23 +0100

    save current progress

--- R/oauth2_google.R
+++ R/oauth2_google.R
@@ -5,12 +5,15 @@
 #'
 #' @param req Request object.
 #' @param res Response object.
-#' @param secret character. This should be the secret that use to sign your JWT. The secret is converted
-#' to raw bytes in the function.
+#' @param client_id character.
+#' @param hd character.
 #'
 #' @importFrom stringr str_remove str_trim
-#' @importFrom jose jwt_decode_hmac
+#' @importFrom jose jwt_split
 #' @importFrom plumber forward
+#' @importFrom anytime anytime
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
 #'
 #' @examples
 #' \dontrun{
@@ -22,28 +25,84 @@
 #' @export
 #'
 
-oauth2_google <- function (req, res, access_token, refresh_token) {
+oauth2_google <- function (req,
+                           res,
+                           client_id,
+                           hd = NULL,
+                           jwks_uri = "https://www.googleapis.com/oauth2/v3/certs") {
 
-  # check if token is valid
+  ## check missing parameters ----------------------------------------------------------------------------
 
-  # try if access token is still working
+  # ensure that the user passed the request object
+  if (missing(req) == TRUE)
+    stop("Please pass the request object.")
 
-  # try to refresh token using refresh token
+  # ensure that the user passed the response object
+  if (missing(res) == TRUE)
+    stop("Please pass the response object.")
+
+  # ensure that the user passed the client_id
+  if (missing(client_id) == TRUE)
+    stop("Please pass the Google client id.")
+
+  ## parse token -----------------------------------------------------------------------------------------
 
   # trim authorization token
   req$HTTP_AUTHORIZATION <- stringr::str_remove(req$HTTP_AUTHORIZATION, "Bearer\\s")
   req$HTTP_AUTHORIZATION <- stringr::str_trim(req$HTTP_AUTHORIZATION)
 
-  # check if token is valid
-  auth <- tryCatch(jose::jwt_decode_hmac(req$HTTP_AUTHORIZATION, secret = secret),
-                   error = function (e) NULL)
+  # parse google's jwt
+  jwt <- jose::jwt_split(req$HTTP_AUTHORIZATION)
 
-  # if token not valid send error
-  if (is.null(auth)) {
+  # add token to request
+  req$jwt <- jwt$payload
+
+  ## check signature -------------------------------------------------------------------------------------
+
+  # download public key file
+  key_file <- httr::GET(jwks_uri)
+  parsed_key_file <- jsonlite::fromJSON(httr::content(key_file, type = "text"))$keys
+
+  # parse public key
+  pub_key <- jose::jwk_read(parsed_key_file[2, ])
+
+  # check signature
+  jose::jwt_decode_sig(req$HTTP_AUTHORIZATION, pub_key)
+
+  ## check jwt payload------------------------------------------------------------------------------------
+
+  # check if iis is correct
+  if (!stringr::str_detect(jwt$payload$iss, "https://accounts.google.com||accounts.google.com")) {
     res$status <- 401
     return(list(status="Failed.",
                 code=401,
                 message="Authentication required."))
+  }
+
+  # check if client id matches
+  if (jwt$payload$aud != client_id) {
+    res$status <- 401
+    return(list(status="Failed.",
+                code=401,
+                message="Authentication required."))
+  }
+
+  # check if token expired
+  if (as.numeric(as.POSIXct(Sys.time())) > jwt$payload$exp) {
+    res$status <- 401
+    return(list(status="Failed.",
+                code=401,
+                message="Authentication required."))
+  }
+
+  # check if hd is valid
+  if (!is.null(hd)) {
+    if (jwt$payload$hd != hd) {
+      res$status <- 401
+      return(list(status="Failed.",
+                  code=401,
+                  message="Authentication required."))
+    }
   }
 
   # redirect to routes
