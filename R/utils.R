ba9500b2c628e001a68bd67dec76a5f9d57b6cc7
Friedrike Preu <friedrike.preu@codecentric.de>
2019-03-15 09:14:22 +0100

    add jwt_split to utils until it is exported in jose

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,22 @@ http_response_list <- function(status, status_code, message){
 auth_required_response <- function(){
   return(http_response_list("Failed.", 401, "Authentication required."))
 }
+
+# adapted from https://github.com/jeroen/jose/blob/master/R/jwt.R until the function is exported in
+# CRAN version (version on github already exports this function)
+jwt_split <- function(jwt){
+  input <- strsplit(jwt, ".", fixed = TRUE)[[1]]
+  stopifnot(length(input) %in% c(2,3))
+  header <- jsonlite::fromJSON(rawToChar(jose::base64url_decode(input[1])))
+  stopifnot(toupper(header$typ) == "JWT")
+  if(is.na(input[3])) input[3] = ""
+  sig <- jose::base64url_decode(input[3])
+  header <- jsonlite::fromJSON(rawToChar(jose::base64url_decode(input[1])))
+  payload <- jsonlite::fromJSON(rawToChar(jose::base64url_decode(input[2])))
+  data <- charToRaw(paste(input[1:2], collapse = "."))
+  if(!grepl("^none|[HRE]S(256|384|512)$", header$alg))
+    stop("Invalid algorithm: ", header$alg)
+  keysize <- as.numeric(substring(header$alg, 3))
+  type <- match.arg(substring(header$alg, 1, 1), c("HMAC", "RSA", "ECDSA"))
+  list(type = type, keysize = keysize, data = data, sig = sig, payload = payload)
+}
