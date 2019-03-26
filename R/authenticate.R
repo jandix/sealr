f071482fe66f50027fff889f8be8ac11307a4df7
Unknown <friedrike.preu@gmail.com>
2019-03-26 08:37:34 +0100

    #18 first draft of authenticate function

---
+++ R/authenticate.R
@@ -0,0 +1,34 @@
+#' authenticate
+#' \code{authenticate} should be used in context of
+#'
+#' @param strategy character. Must be one of: \code{jwt, google}.
+#' @param req plumber request object
+#' @param res plumber response object
+#' @param ... arguments to be passed down to the is_authed_ function.
+#' @export
+authenticate <- function(strategy, req, res, ...){
+
+  function_map <- list(jwt = sealr::is_authed_jwt,
+                       google = sealr::is_authed_google_oauth2)
+
+  strategies <- names(function_map)
+
+  if(length(strategy) > 1) {
+    stop("You can only specify one strategy.")
+  }
+
+  if(!strategy %in% strategies){
+    stop(paste0("strategy argument must be one of: ",
+                paste(strategies, collapse = ", ")))
+  }
+
+  is_authed <- do.call(function_map[[strategy]], args = c(req, res, ...))
+
+  if(!is_authed){
+    sealr::auth_required_response()
+  }
+
+  plumber::forward()
+}
+
+
