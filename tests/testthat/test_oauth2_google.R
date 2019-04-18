d46859bf75a832a802c60e4cb217b8e95745ca94
Friedrike Preu <friedrike.preu@codecentric.de>
2019-04-18 15:38:17 +0200

    #16 try to mock

--- tests/testthat/test_oauth2_google.R
+++ tests/testthat/test_oauth2_google.R
@@ -68,3 +68,52 @@ testthat::test_that("test that the function requires valid HTTP_AUTHORIZATION th
                                         client_id = test_client_id)
   testthat::expect_false(res$is_authed)
 })
+
+
+testthat::test_that("test that the function requires valid HTTP_AUTHORIZATION that matches google key", {
+
+  # generate JWT
+  # test data
+  key <- openssl::rsa_keygen()
+  pub_key <- as.list(key)$pubkey
+  token <- jose::jwt_claim(name = "Franz",
+                           uid = 509)
+  jwt <- jose::jwt_encode_sig(token, key)
+
+
+  jwt_split_up <- jwt_split(jwt)
+  jwt_split_up$header$kid <- "thisismykid"
+
+  # decode and get kid
+  token <- jose::jwt_encode_sig(token, key)
+
+  # download public key file
+  jwks_uri <- "https://www.googleapis.com/oauth2/v3/certs"
+  response <- httr::GET(jwks_uri)
+
+  # TURN INTO TESTTHAT
+  if (httr::http_error(response)) {
+    return(FALSE)
+  }
+  jwks <- jsonlite::fromJSON(httr::content(response, type = "text", encoding = "UTF-8"))$keys
+  jwks$kid <-
+  mockery::stub(sealr::is_authed_oauth2_google, "jsonlite::fromJSON",
+                )
+  # we need to mock the fromJSON method as this is where the dataframe is regturned
+
+  # match kid
+  jwks
+  index <- which(jwks$kid == jwt$header$kid)
+
+
+
+  test_req <- list(HTTP_AUTHORIZATION = jwt)
+  test_res <- list()
+  client_id <- "xxx"
+
+  res <- sealr::is_authed_oauth2_google(req = test_req,
+                                        res = test_res,
+                                        token_location = "header",
+                                        client_id = test_client_id)
+  testthat::expect_false(res$is_authed)
+})
